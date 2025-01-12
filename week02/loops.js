fc536e2118c41f9fbc7d055894a730532dfc8f4f
kukomeza <kukomeza@gmail.com>
2025-01-11 19:50:52 -0600

    loops

--- week02/loops.js
+++ week02/loops.js
@@ -1,7 +1,7 @@
 myInfo = {
     name: "Brother T",
     photo: "images/photo.jpg",
-    favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato"],
+    favoriteFoods: ["Fettucini", "Steak", "Chicken", "Shrimp", "Baked Potato", "Pizza", "Lasagna"],
     hobbies: ["Reading", "Fishing", "Camping"],
     placesLived: [
       {
@@ -18,28 +18,58 @@ myInfo = {
       },
     ],
   };
-  // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
-  let favoriteFood1 = document.createElement("li");
-  favoriteFood1.textContent = myInfo.favoriteFoods[0];
-  
-  let favoriteFood2 = document.createElement("li");
-  favoriteFood2.textContent = myInfo.favoriteFoods[1];
-  
-  let favoriteFood3 = document.createElement("li");
-  favoriteFood3.textContent = myInfo.favoriteFoods[2];
-  
-  let favoriteFood4 = document.createElement("li");
-  favoriteFood4.textContent = myInfo.favoriteFoods[3];
+  const foodsElement = document.querySelector("#favorite-foods");
+const placesElement = document.querySelector("#places-lived");
+// requires a list, and a callback that will produce an html string for each item in the list
+// returns a string of html
+function generateListMarkup(list, templateCallback) {
+  const htmlList = list.map(templateCallback);
+  return htmlList.join("");
+}
+// requires an food string
+// returns that string embedded in HTML markup
+function foodsTemplate(food) {
+  return `<li>${food}</li>`;
+}
 
-  // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
-  document.querySelector("#favorite-foods").appendChild(favoriteFood1);
-  document.querySelector("#favorite-foods").appendChild(favoriteFood2);
-  document.querySelector("#favorite-foods").appendChild(favoriteFood3);
-  document.querySelector("#favorite-foods").appendChild(favoriteFood4);
+// requires an place string
+// returns that string embedded in HTML markup
+function placesTemplate(place) {
+  return `<dt>${place.place}</dt><dd>${place.length}</dd>`;
+}
 
-  
-  function foreach ()
-  {
-   myInfo.hobbies.foreach(); 
-  }
-  
+foodsElement.innerHTML = generateListMarkup(
+  myInfo.favoriteFoods,
+  foodsTemplate
+);
+placesElement.innerHTML = generateListMarkup(
+  myInfo.placesLived,
+  placesTemplate
+);
+
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+for (let i = 0; i < studentReport.length; i++){
+    if (studentReport[i]< LIMIT){
+        console.log(studentReport[i]);
+    }
+}
+
+let i = 0;
+while (i < studentReport.length){
+    if (i < LIMIT){
+        console.log(studentReport[i])  
+    }
+    i++;
+}
+
+studentReport.forEach(function (item)
+    {
+        if (item < LIMIT){
+            console.log(item)
+        }
+    });
+
+    
